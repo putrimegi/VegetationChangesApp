2f5a020cfe8888bbb605cfa87fc730064b50a5fc
YashaCamael <ghiyasashidiqie42@gmail.com>
2022-07-27 00:24:56 +0700

    menambah column

--- pages/5_🔍_About.py
+++ pages/5_🔍_About.py
@@ -10,3 +10,12 @@ GitHub Repository: <https://github.com/giswqs/streamlit-multipage-template>
 """
 )
 
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("Nama Anggota 1")
+    #st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col3:
+    st.header("Nama Angota 2")
+    #st.image("https://static.streamlit.io/examples/dog.jpg")
