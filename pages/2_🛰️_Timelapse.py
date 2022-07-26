5bc2dcde54cbe672043c5f06846393d9b67d1cc0
putrimegi <108792780+putrimegi@users.noreply.github.com>
2022-07-26 23:25:18 +0700

    Update and rename 2_🪟_Split_Map.py to 2_🛰️_Timelapse.py

---
+++ pages/2_🛰️_Timelapse.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+
+st.set_page_config(layout="wide")
+
+st.header("Timelapse")
+st.markdown (
+"""
+Paragraf 1
+Dibawah ini adalah Timelapse vegetasi pulau Kalimantan
+Paragraf 2
+(https://media.giphy.com/media/Ey56FW3ui2TZ2vIe6y/giphy.gif)
+"""
+)
+
