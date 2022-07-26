c25acf949d42647eccb35be779c8b5d9a8ddd3f5
putrimegi <108792780+putrimegi@users.noreply.github.com>
2022-07-26 13:56:58 +0700

    Update Vegetasi Pulau Kalimantan 2001-2020.py

--- pages/Vegetasi Pulau Kalimantan 2001-2020.py
+++ pages/Vegetasi Pulau Kalimantan 2001-2020.py
@@ -11,8 +11,8 @@ import pandas as pd
 #import pygal
 #import ipygee as ui
 
-service_account = 'vegetasi-kalimantan@kerja-praktik-356103.iam.gserviceaccount.com'
-credentials = ee.ServiceAccountCredentials(service_account, 'kerja-praktik-356103-394c75c599d1.json')
+service_account = 'keygoogleearthengine@kp-putri-megi.iam.gserviceaccount.com'
+credentials = ee.ServiceAccountCredentials(service_account, 'kp-putri-megi-86d984e6c787.json')
 ee.Initialize(credentials)
 # In[2]:
 
