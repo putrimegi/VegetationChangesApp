891c65e9663065eda5f80ace22b4583513d642e5
putrimegi <108792780+putrimegi@users.noreply.github.com>
2022-07-26 23:28:39 +0700

    Update 2_🛰️_Timelapse.py

--- pages/2_🛰️_Timelapse.py
+++ pages/2_🛰️_Timelapse.py
@@ -4,9 +4,10 @@ import leafmap.foliumap as leafmap
 st.set_page_config(layout="wide")
 
 st.header("Timelapse")
+st.subheader("This timelapse showed vegetation changes from 2000-2022 on Kalimantan Island")
 st.markdown (
 """
-![This timelapse showed vegetation changes from 2000-2022 on Kalimantan Island](https://media.giphy.com/media/Ey56FW3ui2TZ2vIe6y/giphy.gif)
+![Timelapse Vegetasi Pulau Kalimantan](https://media.giphy.com/media/Ey56FW3ui2TZ2vIe6y/giphy.gif)
 """
 )
 
