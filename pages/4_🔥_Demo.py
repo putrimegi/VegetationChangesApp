2065486e6afc21029398faccd3d7ab6befa7bba4
putrimegi <108792780+putrimegi@users.noreply.github.com>
2022-07-27 00:01:34 +0700

    Update and rename 4_🔥_Heatmap.py to 4_🔥_Demo.py

---
+++ pages/4_🔥_Demo.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+
+st.set_page_config(layout="wide")
+
+st.header("Demo")
+st.markdown(
+"""
+This is demo for our app
+"""
+)
+
+st.video('https://www.youtube.com/watch?v=fTzlyayFXBM&t=684s')
