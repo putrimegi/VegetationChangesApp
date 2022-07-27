2de98fa14a47def1759eeca3cb75de6991b0f9ee
putrimegi <108792780+putrimegi@users.noreply.github.com>
2022-07-27 10:49:15 +0700

    Update 5_🔍_About.py

--- pages/5_🔍_About.py
+++ pages/5_🔍_About.py
@@ -5,19 +5,19 @@ st.set_page_config(layout="wide")
 
 st.markdown(
 """
-Web App URL: <https://template.streamlitapp.com>
-GitHub Repository: <https://github.com/giswqs/streamlit-multipage-template>
+This web app is maintained by ACE,  
+Remote sensing and Geographic Information Science (GIS) researchers group from Geomatics Engineering, Institute Technology of Sepuluh Nopember. 
 """
 )
 
-col1, col2= st.columns(2)
+col1, col2, col3= st.columns(3)
 
 with col1:
-    st.header("Nama Anggota 1")
+    st.header("Member 1")
     #st.image("https://static.streamlit.io/examples/cat.jpg")
 
-with col2:
-    st.header("Nama Angota 2")
+with col3:
+    st.header("Member 2")
     #st.image("https://static.streamlit.io/examples/dog.jpg")
 
 
