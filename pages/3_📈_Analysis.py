48a100702cab8188590f350342152497dbdd0643
putrimegi <108792780+putrimegi@users.noreply.github.com>
2022-07-27 11:01:42 +0700

    Update 3_📈_Analysis.py

--- pages/3_📈_Analysis.py
+++ pages/3_📈_Analysis.py
@@ -3,9 +3,25 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
+st.title('Changes in vegetation area')
+st.header("Graph of changes in the area of vegetation on Kalimantan island, Indonesia in 2000-2022")
 st.markdown(
 """
-This web app is maintained by ACE,  
-Remote sensing and Geographic Information Science (GIS) researchers group from Geomatics Engineering, Institute Technology of Sepuluh Nopember. 
+1. graph (value
+2. description 
+"""
+)
+st.subheader("Vegetation changes in a coal area")
+st.markdown(
+"""
+1. coal image (PT)
+2. description (coal)
+"""
+)
+st.subheader("Vegetation changes")
+st.markdown(
+"""
+1. Vegetation image 
+2. description 
 """
 )
