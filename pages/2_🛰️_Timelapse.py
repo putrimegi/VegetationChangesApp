a216ae61a493f19b9800f932f8e72170edc72492
putrimegi <108792780+putrimegi@users.noreply.github.com>
2022-07-27 14:04:38 +0700

    Update 2_🛰️_Timelapse.py

--- pages/2_🛰️_Timelapse.py
+++ pages/2_🛰️_Timelapse.py
@@ -11,4 +11,7 @@ This timelapse showed vegetation changes from 2000-2022 on Kalimantan Island
 ![Timelapse Vegetasi Pulau Kalimantan](https://media.giphy.com/media/Ey56FW3ui2TZ2vIe6y/giphy.gif)
 """
 )
-
+st.markdown (
+"""
+"""
+  )
