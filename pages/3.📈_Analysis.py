62efdc2fcd66114bd5193bfd50b69780ddb8a93f
putrimegi <108792780+putrimegi@users.noreply.github.com>
2022-07-27 10:54:06 +0700

    Create 3.📈_Analysis.py

---
+++ pages/3.📈_Analysis.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+
+st.set_page_config(layout="wide")
+
+st.markdown(
+"""
+This web app is maintained by ACE,  
+Remote sensing and Geographic Information Science (GIS) researchers group from Geomatics Engineering, Institute Technology of Sepuluh Nopember. 
+"""
+)
