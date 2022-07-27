afc63babe40f4d50fbe786353d8c151dcd283f1d
putrimegi <108792780+putrimegi@users.noreply.github.com>
2022-07-27 15:33:03 +0700

    Update 5_🔍_About.py

--- pages/5_🔍_About.py
+++ pages/5_🔍_About.py
@@ -3,7 +3,10 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
-st.markdown(
+col1, col2, col3= st.columns(3)
+
+with col1:
+    st.markdown(
 """
 This web app is maintained by ACE,  
 Remote sensing and Geographic Information Science (GIS) researchers group from Geomatics Engineering, Institute Technology of Sepuluh Nopember. 
