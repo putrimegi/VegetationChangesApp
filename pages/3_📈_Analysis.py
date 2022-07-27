8f3d2afb7475e188fad98c3c0fe331ba194b7c42
putrimegi <108792780+putrimegi@users.noreply.github.com>
2022-07-27 21:48:00 +0700

    Update 3_📈_Analysis.py

--- pages/3_📈_Analysis.py
+++ pages/3_📈_Analysis.py
@@ -18,8 +18,8 @@ st.markdown(
 ![](https://i.postimg.cc/CLc2jBVD/Area-of-Vegetation.jpg)
 """
 )
-st.markdown(
-"""
+expander = st.expander("See explanation")
+expander.write("""
 The graph above shows the total area of four vegetation classes on the island of Kalimantan from 2000 to 2021. The no vegetation and slightly density classes rarely experience small changes, the moderately density class is decreasing, and the high density class is increasing.
 """
 )
@@ -62,19 +62,20 @@ st.markdown(
 ![](https://i.postimg.cc/Zqj0ZzB6/9.jpg)
 """
 )
-st.markdown(
-"""
-The island of Kalimantan is famous for it's mineral wealth and many mining industries operate. Through the picture above, the increase in a mining areas from 2000 to 2021 can be seen.
+expander = st.expander("See explanation")
+expander.write("""
+  The island of Kalimantan is famous for it's mineral wealth and many mining industries operate. Through the picture above, the increase in a mining areas from 2000 to 2021 can be seen.
 """
 )
+
 st.subheader("📌 Vegetation Changes")
 st.markdown(
 """
 ![](https://i.postimg.cc/3R3FRhx1/pnas-1704728114fig01.jpg) 
 """
 )
-st.markdown(
-"""
-Changes in vegetation every year are increasing. This change is presumably due to land clearing for plantations, timber industries, and other industries that utilize natural resources in Kalimantan. The image above is a map of the oil palm plantation area in 2017. The plantation areas are spread across the island of Kalimantan. Image sourced from [Kimberly M. Carlson,2017](https://www.pnas.org/doi/10.1073/pnas.1704728114#fig01)
+expander = st.expander("See explanation")
+expander.write("""
+  Changes in vegetation every year are increasing. This change is presumably due to land clearing for plantations, timber industries, and other industries that utilize natural resources in Kalimantan. The image above is a map of the oil palm plantation area in 2017. The plantation areas are spread across the island of Kalimantan. Image sourced from [Kimberly M. Carlson,2017](https://www.pnas.org/doi/10.1073/pnas.1704728114#fig01)
 """
 )
