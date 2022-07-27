694748049d3e9f3227746bfaf7cbcca3c2f7b1bd
putrimegi <108792780+putrimegi@users.noreply.github.com>
2022-07-27 10:12:55 +0700

    Update 4_❓_Demo.py

--- pages/4_❓_Demo.py
+++ pages/4_❓_Demo.py
@@ -9,7 +9,7 @@ st.title('Demo')
 st.markdown(
 """
 This is a demonstration of our web apps created using [streamlit](https://streamlit.io/) and [leafmapapp](https://leafmap.org/) 
-See this video bellow *How to use and load our vegetation maps* in [Kalimantan Island](https://id.wikipedia.org/wiki/Kalimantan) 
+.See this video bellow *How to use and load our vegetation maps* in [Kalimantan Island](https://id.wikipedia.org/wiki/Kalimantan) 
 """
 )
 
