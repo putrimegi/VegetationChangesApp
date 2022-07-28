797f77170cc055822e2e712eac3d37b539d0c5cc
YashaCamael <ghiyasashidiqie42@gmail.com>
2022-07-28 21:15:06 +0700

    menggunakan leafmap

--- pages/7_📍_edit.py
+++ pages/7_📍_edit.py
@@ -3,17 +3,19 @@ import ee
 import numpy as np
 import geemap
 import pandas as pd
+import leafmap.foliumap as leafmap
 
-service_account = 'keygoogleearthengine@kp-putri-megi.iam.gserviceaccount.com'
-credentials = ee.ServiceAccountCredentials(service_account, 'kp-putri-megi-86d984e6c787.json')
-ee.Initialize(credentials)
+# service_account = 'keygoogleearthengine@kp-putri-megi.iam.gserviceaccount.com'
+# credentials = ee.ServiceAccountCredentials(service_account, 'kp-putri-megi-86d984e6c787.json')
+# ee.Initialize(credentials)
 
 st.set_page_config(layout="wide")
 
 st.title("Marker Cluster")
 
-Map = geemap.Map()
-Map.setCenter(114.0, 5.0, 6)
+Map = leafmap.Map(center=[114.0, 5.0], zoom=6)
+#Map = geemap.Map()
+#Map.setCenter(114.0, 5.0, 6)
 
 marks = 'Database_Titik_Lokasi_Kalimantan.csv'
 
