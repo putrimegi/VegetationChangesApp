7759a9cf68481af16a58f144d317b1c61c841a5b
putrimegi <108792780+putrimegi@users.noreply.github.com>
2022-07-28 09:02:17 +0700

    Update 7_📍_edit.py

--- pages/7_📍_edit.py
+++ pages/7_📍_edit.py
@@ -15,22 +15,22 @@ st.sidebar.image(logo)
 
 st.title("Marker Cluster")
 
-with st.expander("See source code"):
-    with st.echo():
+# with st.expander("See source code"):
+#     with st.echo():
 
-         m = leafmap.Map(center=[40, -100], zoom=4)
-#         cities = 'https://github.com/putrimegi/kerjapraktek/blob/5cbd738e3c96e0b89fe2f8cb8357838fa24845ee/Titik%20Lokasi%20Kalimantan.csv'
-#         #regions = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson'
+#          m = leafmap.Map(center=[40, -100], zoom=4)
+# #         cities = 'https://github.com/putrimegi/kerjapraktek/blob/5cbd738e3c96e0b89fe2f8cb8357838fa24845ee/Titik%20Lokasi%20Kalimantan.csv'
+# #         #regions = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson'
 
-#         #m.add_geojson(regions, layer_name='US Regions')
-#         m.add_points_from_xy(
-#             cities,
-#             x="Longitude",
-#             y="Latitude",
-#             color_column='Type',
-#             icon_names=['gear', 'map'],
-#             spin=True,
-#             add_legend=True,
-#         )
+# #         #m.add_geojson(regions, layer_name='US Regions')
+# #         m.add_points_from_xy(
+# #             cities,
+# #             x="Longitude",
+# #             y="Latitude",
+# #             color_column='Type',
+# #             icon_names=['gear', 'map'],
+# #             spin=True,
+# #             add_legend=True,
+# #         )
         
- m.to_streamlit(height=700)
+#  m.to_streamlit(height=700)
