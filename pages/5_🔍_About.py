3e4e67e84b5dfdf35bbe72478951f89837100e51
putrimegi <108792780+putrimegi@users.noreply.github.com>
2022-07-28 09:30:09 +0700

    Update 5_🔍_About.py

--- pages/5_🔍_About.py
+++ pages/5_🔍_About.py
@@ -24,21 +24,21 @@ st.markdown(
 coll1, coll2, coll3= st.columns(3)
 
 with coll1:
-    st.header("Member 1")
+    st.subheader("Member 1")
     #st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with coll3:
-    st.header("Member 2")
+    st.subheader("Member 2")
     st.markdown(
         """
         Megivareza Putri Hanansyah
         03311940000041
-        email : varezamegi@gmail.com
-        instagram : @megivareza
-        Linked in : megivareza
         """
     )
-    #st.image("https://i.postimg.cc/CLDfTLFr/Jurnalistik-Megivareza-Putri-Hanansyah-03311940000041.jpg")
+    st.text('varezamegi@gmail.com')
+    st.text('[LinkedIn](https://www.linkedin.com/in/megivareza-putri-hanansyah-b6b4b720a/)')
+    st.text('[Instagram](https://instagram.com/megivareza?igshid=YmMyMTA2M2Y=)')
+    st.image("https://i.postimg.cc/CLDfTLFr/Jurnalistik-Megivareza-Putri-Hanansyah-03311940000041.jpg")
 
 
 st.header("batas bawah")
