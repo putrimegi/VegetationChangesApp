ef2dd1182019d196f1c74136cca5a38d352381b8
ACE <108792780+putrimegi@users.noreply.github.com>
2022-08-03 11:37:27 +0700

    Update 4_❓_Demo.py

--- pages/4_❓_Demo.py
+++ pages/4_❓_Demo.py
@@ -11,5 +11,11 @@ st.markdown(
 See this video demonstration of our web apps created using [streamlit](https://streamlit.io/) and [leafmapapp](https://leafmap.org/) below to learn *How to use and load our Vegetation Maps* on [Kalimantan Island](https://id.wikipedia.org/wiki/Kalimantan).
 """
 )
+st.subheader('Demonstration of Vegetation changes web application')
 st.video('https://www.youtube.com/watch?v=pkQDFjMkAZw&t=14s')
+st.markdown(
+"""
+"""
+)
+st.subheader('Developing a Streamlit Web App for Geospasial Application')
 st.video('https://www.youtube.com/watch?v=fTzlyayFXBM&t=684s')
